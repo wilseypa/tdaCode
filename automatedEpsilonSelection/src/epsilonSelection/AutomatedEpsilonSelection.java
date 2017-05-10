1c927af2d3a49d75d3c77d45344089d7db1dd2c2
Anindya Moitra <id.anindya@gmail.com>
2017-05-10 13:10:42 -0400

    Take one vector at a time and find its k-nearest neighbors. Need to convert java arrayLists to weka instances, or to use weka to read the csv data file itself.

--- automatedEpsilonSelection/src/epsilonSelection/AutomatedEpsilonSelection.java
+++ automatedEpsilonSelection/src/epsilonSelection/AutomatedEpsilonSelection.java
@@ -2,6 +2,7 @@ package epsilonSelection;
 
 import java.io.FileReader;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import com.opencsv.CSVReader;
 
@@ -15,14 +16,18 @@ public class AutomatedEpsilonSelection {
 	public static void main(String[] args) throws Exception {
 //		Read an input csv file. Place the file in the project directory.
 //		Build reader instance
-		CSVReader reader = new CSVReader(new FileReader("dataSet.csv"), ',', '"', 1);
+		CSVReader reader = new CSVReader(new FileReader("dataSet.csv"), ',', '"', 0);
 //		Read all rows at once
 		List<String[]> allRows = reader.readAll();
-//		Append an order = 0 to each row.
-		for(String[] row : allRows) {
-			List<String> rowAsArrayList = new ArrayList<String>(Arrays.asList(row));
-			rowAsArrayList.add("0");
-			System.out.println(rowAsArrayList);
+		long order = 0;
+//		Take one vector at a time and find its k-nearest neighbors.
+		for(String[] arrayRow : allRows) {
+			order = order + 1;
+			String stringOrder = String.valueOf(order);
+			List<String> arrayListRow = new ArrayList<String>(Arrays.asList(arrayRow));
+			arrayListRow.add(stringOrder);
+			List<Double> row = arrayListRow.stream().map(Double::valueOf).collect(Collectors.toList());
+			System.out.println(row);
 		}
 			
 //	        System.out.println(Arrays.toString(row));
